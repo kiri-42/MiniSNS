100e9f63a784e2fe491cc407be7d1dd99d1c5ac3
kiri0314 <t.kirihara0314@gmail.com>
2022-06-16 04:43:20 +0900

    ref: DBの初期化insert文を変更

--- problem1/mysql/0_init.sql
+++ problem1/mysql/0_init.sql
@@ -19,15 +19,11 @@ CREATE TABLE `block_list` (
   PRIMARY KEY (`id`)
 );
 
-INSERT INTO `users` VALUES
-  (1, 1, 'yamada'), (2, 2, 'tanaka'), (3, 3, 'katou'),
-  (4, 4, 'satou'), (5, 5, 'kobayashi'), (6, 6, 'suzuki'), (7, 7, 'takahashi'), (8, 8, 'itou'), (9, 9, 'watanabe'), (10, 10, 'yamamoto'), (11, 11, 'yoshida'), (12, 12, 'yamada'), (13, 13, 'sasaki'), (14, 14, 'yamaguchi'), (15, 15, 'matsumoto'), (16, 16, 'inoue'), (17, 17, 'kimura');
+INSERT INTO `users` (`user_id`, `name`) VALUES
+  (1, 'yamada'), (2, 'tanaka'), (3, 'katou'), (4, 'satou'), (5, 'kobayashi'), (6, 'suzuki'), (7, 'takahashi'), (8, 'itou'), (9, 'watanabe'), (10, 'yamamoto'), (11, 'yoshida'), (12, 'yamada'), (13, 'sasaki'), (14, 'yamaguchi'), (15, 'matsumoto'), (16, 'inoue'), (17, 'kimura');
 
-INSERT INTO `friend_link` VALUES
-  (1, 1, 2), (2, 1, 3), (3, 1, 4), (4, 1, 5), (5, 1, 6),(6, 1, 7), (7, 1, 8), (8, 1, 9), (9, 1, 10), (10, 1, 11), (11, 1, 12), (12, 1, 13), (13, 1, 14), (14, 1, 14), (15, 2, 3), (16, 3, 5), (17, 8, 1), (18, 9, 13), (19, 3, 9), (20, 10, 11), (21, 7, 8);
+INSERT INTO `friend_link` (`user1_id`, `user2_id`) VALUES
+  (1, 2), (1, 3), (1, 4), (1, 5), (1, 6),(1, 7), (1, 8), (1, 9), (1, 10), (1, 11), (1, 12), (1, 13), (1, 14), (1, 14), (2, 3), (3, 5), (8, 1), (9, 13), (3, 9), (10, 11), (7, 8);
 
-INSERT INTO `block_list` VALUES
-  (1, 2, 4), (2, 1, 3);
--- INSERT INTO `users` (`user_id`, `name`) VALUES (1, 'yamda')
--- INSERT INTO `users` (`user_id`, `name`) VALUES (2, 'tanaka')
-
+INSERT INTO `block_list` (`user1_id`, `user2_id`) VALUES
+  (2, 4), (1, 3);
