2c50233b9e69cd04c34abaadbbaa884b3acb28af
kiri0314 <t.kirihara0314@gmail.com>
2022-06-20 13:53:39 +0900

    ref: NewRouterをhandlerパッケージに移動

--- problem1/app/go/main.go
+++ problem1/app/go/main.go
@@ -1,19 +1,16 @@
 package main
 
 import (
-	"database/sql"
+	_ "database/sql"
 	"fmt"
 	"os"
 	"strconv"
 
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	_ "github.com/labstack/echo/v4"
 
 	"problem1/configs"
-	"problem1/infrastructure"
 	"problem1/interface/handler"
-	"problem1/usecase"
 )
 
 func main() {
@@ -26,26 +23,11 @@ func main() {
 	}
 	defer db.Close()
 
-	e := newRouter(db)
+	e := handler.NewRouter(db)
 	if err != nil {
 		fmt.Fprintln(os.Stdout, err.Error())
 		return
 	}
 
 	e.Logger.Fatal(e.Start(":" + strconv.Itoa(conf.Server.Port)))
-}
-
-func newRouter(db *sql.DB) *echo.Echo {
-	userRepository := infrastructure.NewUserRepository(db)
-	userUsecase := usecase.NewUserUsecase(userRepository)
-	userHandler := handler.NewUserHandler(userUsecase)
-
-	e := echo.New()
-	e.HTTPErrorHandler = handler.HttpErrorHandler
-	e.Use(middleware.Recover()) // httpハンドラ内でpanicしてもサーバーが落ちないようにする
-	e.Use(middleware.Logger())  // httpリクエストのロクを出力
-
-	handler.Routing(e, userHandler)
-
-	return e
 }
